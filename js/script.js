309df8c255b48106daccd8cf94601149aae38bc9
jpedrorc <op.carvalho11@gmail.com>
2021-03-18 03:29:43 -0300

    Create list of names

---
+++ js/script.js
@@ -0,0 +1,51 @@
+window.addEventListener("load", start);
+
+var globalNames = ["Um", "Dois", "Três", "Quatro"];
+var inputName = null;
+console.log(globalNames.length)
+function start() {
+    inputName  = document.querySelector('#inputName')
+    preventFormSubmit();
+    activateInput()
+    render();
+}
+
+function preventFormSubmit(){
+    function handleFormSubmit(event){
+        event.preventDefault();
+    }
+    
+    var form = document.querySelector('form');
+    form.addEventListener('submit', handleFormSubmit);
+}
+
+function activateInput(){
+    function insertName(newName){
+        globalNames.push(newName);
+        render();
+    }
+    function handleTyping(event){
+        if (event.key === 'Enter'){
+            insertName(event.target.value);
+
+        }
+    }
+    inputName.addEventListener('keyup', handleTyping)
+    inputName.focus();
+}
+
+function render(){
+    var divNames = document.querySelector("#names");
+
+    var ul = document.createElement('ul');
+    
+    for (let i = 0; i < globalNames.length; i++) {
+        var currentName = globalNames[i];
+
+        var li = document.createElement('li');
+        li.textContent = currentName;
+        ul.appendChild(li);
+    }
+
+    divNames.appendChild(ul);
+}
