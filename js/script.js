048b955941573c94cd405bc00de2116779731d7f
jpedrorc <op.carvalho11@gmail.com>
2021-03-18 23:41:44 -0300

    End of the project

--- js/script.js
+++ js/script.js
@@ -1,51 +1,112 @@
-window.addEventListener("load", start);
+window.addEventListener('load', start);
 
-var globalNames = ["Um", "Dois", "Três", "Quatro"];
+var globalNames = ['Um', 'Dois', 'Três', 'Quatro'];
 var inputName = null;
-console.log(globalNames.length)
+var isEditing = false;
+var currentIndex = null;
+
 function start() {
-    inputName  = document.querySelector('#inputName')
-    preventFormSubmit();
-    activateInput()
-    render();
+  inputName = document.querySelector('#inputName');
+  preventFormSubmit();
+  activateInput();
+  render();
 }
 
-function preventFormSubmit(){
-    function handleFormSubmit(event){
-        event.preventDefault();
-    }
-    
-    var form = document.querySelector('form');
-    form.addEventListener('submit', handleFormSubmit);
+function preventFormSubmit() {
+  function handleFormSubmit(event) {
+    event.preventDefault();
+  }
+
+  var form = document.querySelector('form');
+  form.addEventListener('submit', handleFormSubmit);
 }
 
-function activateInput(){
-    function insertName(newName){
-        globalNames.push(newName);
-        render();
+function activateInput() {
+  function insertName(newName) {
+    if (newName === '') {
+      emptyName();
+      return;
     }
-    function handleTyping(event){
-        if (event.key === 'Enter'){
-            insertName(event.target.value);
-
-        }
-    }
-    inputName.addEventListener('keyup', handleTyping)
-    inputName.focus();
-}
-
-function render(){
-    var divNames = document.querySelector("#names");
-
-    var ul = document.createElement('ul');
-    
     for (let i = 0; i < globalNames.length; i++) {
-        var currentName = globalNames[i];
-
-        var li = document.createElement('li');
-        li.textContent = currentName;
-        ul.appendChild(li);
+      if (globalNames[i] === newName) {
+        alert('Este nome já existe');
+        clearInput();
+        return;
+      }
     }
+    globalNames.push(newName);
+    render();
+  }
 
-    divNames.appendChild(ul);
+  function updateName(newName) {
+    globalNames[currentIndex] = newName;
+  }
+
+  function handleTyping(event) {
+    if (event.key === 'Enter') {
+      if (isEditing) {
+        updateName(event.target.value);
+      } else {
+        insertName(event.target.value);
+      }
+      isEditing = false;
+      render();
+      clearInput();
+    }
+  }
+  inputName.addEventListener('keyup', handleTyping);
+  inputName.focus();
+}
+
+function render() {
+  function createDeleteButton(index) {
+    function deleteName() {
+      globalNames.splice(index, 1);
+      render();
+    }
+    var button = document.createElement('button');
+    button.classList.add('deleteButton');
+    button.textContent = 'x';
+    button.addEventListener('click', deleteName);
+    return button;
+  }
+
+  function createSpan(name, index) {
+    function editItem() {
+      inputName.value = name;
+      inputName.focus();
+      isEditing = true;
+      currentIndex = index;
+    }
+    var span = document.createElement('span');
+    span.classList.add('clickable');
+    span.textContent = name;
+    span.addEventListener('click', editItem);
+    return span;
+  }
+  var divNames = document.querySelector('#names');
+  divNames.innerHTML = '';
+  var ul = document.createElement('ul');
+  for (let i = 0; i < globalNames.length; i++) {
+    var currentName = globalNames[i];
+    var li = document.createElement('li');
+    var button = createDeleteButton(i);
+    var span = createSpan(currentName, i);
+    li.appendChild(button);
+    li.appendChild(span);
+    ul.appendChild(li);
+  }
+  divNames.appendChild(ul);
+  clearInput();
+}
+
+function clearInput() {
+  inputName.value = '';
+  inputName.focus();
+}
+
+function emptyName() {
+  alert('O campo está vazio');
+  inputName.focus();
+  render();
 }
